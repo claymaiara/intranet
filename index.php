39af6d7fdf737ed89c90301fc218d97f43284d15
Clay Maiara <clay_maiara@hotmail.com>
2014-04-29 17:44:53 -0300

    Inicio da pagina index

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php
+<!doctype html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>INTRANET CEADIS</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-default navbar-static-top" role="navigation">
+			<p class="navbar-text">CEADIS - Centro Estadual de Armazenamento e
+				Distribuição de insumos de Saúde</p>
+		</nav>
+		<div class="panel panel-default col-sm-3">
+			<div class="panel-heading">
+				<h3 class="panel-title">LINK DE ACESSO</h3>
+			</div>
+			<div class="panel-body">
+			<ul class="">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Alterar Senha</a></li>
+					</ul>
+			</div>
+
+			<footer>
+				<nav class="navbar navbar-default navbar-fixed-bottom"
+					role="navigation">
+					<p class="navbar-text">© CEADIS - 2014
+				
+				</nav>
+			</footer>
+		</div>
+</div>
+</body>
+</html>
