5aab6b5469e46c6e5795850bb503bb36867710c0
Clay Maiara <clay_maiara@hotmail.com>
2014-04-30 14:27:16 -0300

    Alteração e inclusão de links no index

--- index.php
+++ index.php
@@ -8,34 +8,38 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-default navbar-static-top" role="navigation">
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 		<h4 class="navbar-text">CEADIS - Centro Estadual de Armazenamento e
 			Distribuição de insumos de Saúde</h4>
 	</nav>
-	<br>
-	<div class="container-fluid">
-		<div class="col-sm-4">
-			<div class="panel panel-default col-sm-offset-1 col-sm-10">
+	<nav class="navbar"></nav>
+
+	<div class="container-fluid ">
+		<div class="col-sm-3">
+			<div class="panel panel-default">
 				<div class="panel-heading">
 					<h3 class="panel-title">LINK DE ACESSO</h3>
 				</div>
-
-				<ul class="panel-body list-unstyled list-group">
-					<li><a href="http://172.22.0.42/armhazena/" class="list-group-item-primary ">WMS
-							- Armhazena</a></li>
-					<li><a href="#" >Portal - Estoque Web</a></li>
-					<li><a href="#" >Portal - Sorocaba</a></li>
-					<li><a href="#">SGA - Sistema de Gestão de
-							Atividade</a></li>
-					<li><a href="#">Solicitação de Materiais</a></li>
-					<li><a href="#">Portal - RH</a></li>
-					<li><a href="#" >Pesquisas ADAIA</a></li>
+				<div class="panel-body">
+					<ul class="list-unstyled">
+						<li><a href="http://172.22.0.42/armhazena">WMS - Armhazena</a></li>
+						<li><a href="http://172.22.0.33/ceadis">Portal - Estoque Web</a></li>
+						<li><a href="http://177.184.203.235:8088/estoque/">Portal -
+								Sorocaba</a></li>
+						<li><a href="http://172.22.0.30/SGA">SGA - Sistema de Gestão de
+								Atividade</a></li>
+						<li><a href="http://172.22.0.46/estoque/">Solicitação de Materiais</a></li>
+						<li><a href="http://187.48.62.186:81/corpore.net/Login.aspx">Portal
+								- RH</a></li>
+						<li><a href="http://192.168.31.250/">Pesquisas ADAIA</a></li>
 				</ul>
-			<div class="panel-footer"></div>
 			</div>
-
+			<div class="panel-footer"></div>
 		</div>
-		<div class="jumbotron col-sm-8">
+
+	</div>
+	<div class="container col-sm-9">
+		<div class="jumbotron ">
 			<h1>
 				<b>CEADIS</b>
 			</h1>
@@ -45,6 +49,7 @@
 				alt="Logotipo Ceadis">
 
 		</div>
+	</div>
 
 
 	</div>
@@ -54,6 +59,7 @@
 			<p class="navbar-text">© CEADIS - 2014
 		
 		</nav>
+		<nav class="navbar"></nav>
 	</footer>
 
 
