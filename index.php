a13dcb9b453578d58626d8d89047a82153703975
Clay Maiara <clay_maiara@hotmail.com>
2014-04-30 10:43:22 -0300

    Conmtinuação da pegina index, foi addicionado os paineis

--- index.php
+++ index.php
@@ -7,31 +7,56 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+
+	<nav class="navbar navbar-default navbar-static-top" role="navigation">
+		<h4 class="navbar-text">CEADIS - Centro Estadual de Armazenamento e
+			Distribuição de insumos de Saúde</h4>
+	</nav>
+	<br>
 	<div class="container-fluid">
-		<nav class="navbar navbar-default navbar-static-top" role="navigation">
-			<p class="navbar-text">CEADIS - Centro Estadual de Armazenamento e
-				Distribuição de insumos de Saúde</p>
-		</nav>
-		<div class="panel panel-default col-sm-3">
-			<div class="panel-heading">
-				<h3 class="panel-title">LINK DE ACESSO</h3>
-			</div>
-			<div class="panel-body">
-			<ul class="">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Alterar Senha</a></li>
-					</ul>
+		<div class="col-sm-4">
+			<div class="panel panel-default col-sm-offset-1 col-sm-10">
+				<div class="panel-heading">
+					<h3 class="panel-title">LINK DE ACESSO</h3>
+				</div>
+
+				<ul class="panel-body list-unstyled list-group">
+					<li><a href="http://172.22.0.42/armhazena/" class="list-group-item-primary ">WMS
+							- Armhazena</a></li>
+					<li><a href="#" >Portal - Estoque Web</a></li>
+					<li><a href="#" >Portal - Sorocaba</a></li>
+					<li><a href="#">SGA - Sistema de Gestão de
+							Atividade</a></li>
+					<li><a href="#">Solicitação de Materiais</a></li>
+					<li><a href="#">Portal - RH</a></li>
+					<li><a href="#" >Pesquisas ADAIA</a></li>
+				</ul>
+			<div class="panel-footer"></div>
 			</div>
 
-			<footer>
-				<nav class="navbar navbar-default navbar-fixed-bottom"
-					role="navigation">
-					<p class="navbar-text">© CEADIS - 2014
-				
-				</nav>
-			</footer>
 		</div>
-</div>
+		<div class="jumbotron col-sm-8">
+			<h1>
+				<b>CEADIS</b>
+			</h1>
+			<p>Ao lado você encontra os links de acesso aos principais sistemas
+				do CEADIS.</p>
+			<img src="img/fotoinicial.png" class="img-responsive img-rounded"
+				alt="Logotipo Ceadis">
+
+		</div>
+
+
+	</div>
+	<footer>
+		<nav class="navbar navbar-default navbar-fixed-bottom"
+			role="navigation">
+			<p class="navbar-text">© CEADIS - 2014
+		
+		</nav>
+	</footer>
+
+
+
 </body>
 </html>
