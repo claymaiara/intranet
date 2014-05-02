0ac65587ba5335002e413fdf4c5ac633360e8900
marciomrs4 <marciomrs4@hotmail.com>
2014-05-02 11:05:20 -0300

    Criado a separacao dos link em um unico arquivo e alterado o funcionamento para abrir sempre em uma nova aba

---
+++ links.php
@@ -0,0 +1,7 @@
+<li><a href="http://172.22.0.42/armhazena" target="_blank">WMS - Armhazena</a></li>
+<li><a href="http://172.22.0.33/ceadis" target="_blank">Portal - Estoque Web</a></li>
+<li><a href="http://177.184.203.235:8088/estoque/" target="_blank">Portal - Sorocaba</a></li>
+<li><a href="http://172.22.0.30/SGA" target="_blank">SGA - Sistema de Gestão de Atividade</a></li>
+<li><a href="http://172.22.0.46/estoque/" target="_blank">Solicitação de Materiais</a></li>
+<li><a href="http://187.48.62.186:81/corpore.net/Login.aspx" target="_blank">Portal - RH</a></li>
+<li><a href="http://192.168.31.250/" target="_blank">Pesquisas ADAIA</a></li>
